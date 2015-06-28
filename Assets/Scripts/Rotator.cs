e02bc63ce8ce329357ba582b8989b2dd88748fa1
Janitha Samarasinghe <jcsam93@gmail.com>
2015-06-28 15:21:57 +0530

    First Commit
    Created unity project MainGame and MainMenu scenes

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	public float speedx, speedy, speedz;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up, speedx * Time.deltaTime);
+		transform.Rotate (Vector3.right, speedy * Time.deltaTime);
+		transform.Rotate (Vector3.forward, speedz * Time.deltaTime);
+	}
+}
