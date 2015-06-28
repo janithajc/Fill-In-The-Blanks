3837e12bc793e6d08bbb62f639f3e040d98a8cdd
Janitha Samarasinghe <jcsam93@gmail.com>
2015-06-28 16:51:22 +0530

    Added Some functionality
    Menu buttons, Moon mouse rotator

---
+++ Assets/Scripts/MainMenu/StartButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class StartButton : MonoBehaviour, IPointerClickHandler {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnPointerClick(PointerEventData d){
+		Application.LoadLevel ("MainGame");
+	}
+}
