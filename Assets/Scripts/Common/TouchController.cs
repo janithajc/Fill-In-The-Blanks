431f31d26577a19e02df0f55a0c49a58160e92de
Janitha Samarasinghe <jcsam93@gmail.com>
2015-07-27 22:33:50 +0530

    Added boundaries
    Now the game objects do not fly off into the vastness of space

--- Assets/Scripts/Common/TouchController.cs
+++ Assets/Scripts/Common/TouchController.cs
@@ -17,13 +17,13 @@ public class TouchController : MonoBehaviour {
 	private Vector3 initialPosition;
 	private Vector3 offset;
 	private Rigidbody rbody;
-	private Color semiTransparent;
+	private Color gold;
 
 	void Start (){
 		Input.simulateMouseWithTouches = true;
 		rend = GetComponent<Renderer> ();
 		rbody = GetComponent<Rigidbody> ();
-		semiTransparent = new Color (1, 0.7f, 0.5f);
+		gold = new Color (1, 0.7f, 0.5f);
 	}
 
 	void OnMouseDown() {
@@ -34,18 +34,18 @@ public class TouchController : MonoBehaviour {
 		initialPosition = transform.position;
 		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 		Debug.Log ("Touched "+name);
-		if(rbody)
+		if(rbody != null)
 			rbody.velocity = Vector3.zero;
 	}
 
 	void OnMouseDrag() { 
 		if (isGonnaDrag && Time.time > touchTime + 1F) {
-			rend.material.SetColor("_Color", semiTransparent);
+			rend.material.SetColor("_Color", gold);
 			Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 			Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
 			transform.position = curPosition;
 			isDragging = false;
-			if(rbody)
+			if(rbody != null)
 				rbody.velocity = curPosition - initialPosition;
 		}
 	}	
@@ -64,6 +64,7 @@ public class TouchController : MonoBehaviour {
 		transform.position = new Vector3 (transform.position.x, transform.position.y, -2);
 		if (Input.GetMouseButton(0) && isDragging) {
 			theSpeed = new Vector3(-Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0.0F);
+			//Debug.Log(-Input.GetAxis("Mouse X") + " : " + Input.GetAxis("Mouse Y"));
 			avgSpeed = Vector3.Lerp(avgSpeed, theSpeed, Time.deltaTime * 5);
 		} else {
 			if (isDragging) {
@@ -73,64 +74,7 @@ public class TouchController : MonoBehaviour {
 			float i = Time.deltaTime * lerpSpeed;
 			theSpeed = Vector3.Lerp(theSpeed, Vector3.zero, i);
 		}
-		
 		transform.Rotate(Camera.main.transform.up * theSpeed.x * rotationSpeed, Space.World);
 		transform.Rotate(Camera.main.transform.right * theSpeed.y * rotationSpeed, Space.World);
 	}
 }
-
-/*using UnityEngine;
-using System.Collections;
-
-public class RotationController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	bool isGrabbed = false;
-	Vector3 grabbedPoint;
-	
-	void Update () 
-	{ 
-		RaycastHit hit; 
-		
-		if (Input.GetMouseButton(0)) 
-		{         
-			if(!isGrabbed)
-			{
-				isGrabbed = true;
-				
-				//Find the hit point
-				Physics.Raycast (Camera.main.ScreenPointToRay(Input.mousePosition), out hit);
-				
-				//Convert touched point to a local point on the earth
-				grabbedPoint = getTouchedPointLocal();
-			}
-			else
-			{
-				Vector3 targetPoint = getTouchedPoint(); 
-				transform.rotation = Quaternion.FromToRotation (grabbedPoint,targetPoint);
-			}
-		}
-		else
-			isGrabbed = false;
-	}
-	
-	Vector3 getTouchedPoint()
-	{
-		RaycastHit hit;
-		
-		//Find the hit point
-		Physics.Raycast (Camera.main.ScreenPointToRay(Input.mousePosition), out hit);
-		
-		//Convert touched point to a local point on the earth
-		return  hit.point-transform.position; 
-	}
-	
-	Vector3 getTouchedPointLocal()
-	{
-		return transform.InverseTransformPoint(getTouchedPoint());
-	}
-}*/
