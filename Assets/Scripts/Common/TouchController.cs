cd0e5f0ae07cc207c7ec2e315df2652d8fbf250e
Janitha Samarasinghe <jcsam93@gmail.com>
2015-07-27 21:49:41 +0530

    Added physics
    Added rigidbodies, velocities, new color on drag

--- Assets/Scripts/Common/TouchController.cs
+++ Assets/Scripts/Common/TouchController.cs
@@ -13,32 +13,42 @@ public class TouchController : MonoBehaviour {
 	private Vector3 targetSpeedX;
 	private float touchTime;
 	private Renderer rend;
+	private Vector3 screenPoint;
+	private Vector3 initialPosition;
+	private Vector3 offset;
+	private Rigidbody rbody;
+	private Color semiTransparent;
 
 	void Start (){
 		Input.simulateMouseWithTouches = true;
-		rend = GetComponent<Renderer>();
+		rend = GetComponent<Renderer> ();
+		rbody = GetComponent<Rigidbody> ();
+		semiTransparent = new Color (1, 0.7f, 0.5f);
 	}
 
-	private Vector3 screenPoint; private Vector3 offset;
-	
-	void OnMouseDrag() { 
-		if (isGonnaDrag && Time.time > touchTime + 2F) {
-			rend.material.SetColor("_Color",Color.green);
-			Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-			Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-			transform.position = curPosition;
-			isDragging = false;
-		}
-	}
-	
 	void OnMouseDown() {
 		isDragging = true;
 		isGonnaDrag = true;
 		touchTime = Time.time;
 		//screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position); // I removed this line to prevent centring 
+		initialPosition = transform.position;
 		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 		Debug.Log ("Touched "+name);
+		if(rbody)
+			rbody.velocity = Vector3.zero;
 	}
+
+	void OnMouseDrag() { 
+		if (isGonnaDrag && Time.time > touchTime + 1F) {
+			rend.material.SetColor("_Color", semiTransparent);
+			Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+			Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+			transform.position = curPosition;
+			isDragging = false;
+			if(rbody)
+				rbody.velocity = curPosition - initialPosition;
+		}
+	}	
 
 	void OnMouseExit() {
 		isGonnaDrag = false;
@@ -51,6 +61,7 @@ public class TouchController : MonoBehaviour {
 	
 	void Update() {
 		//Rotation Detect
+		transform.position = new Vector3 (transform.position.x, transform.position.y, -2);
 		if (Input.GetMouseButton(0) && isDragging) {
 			theSpeed = new Vector3(-Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0.0F);
 			avgSpeed = Vector3.Lerp(avgSpeed, theSpeed, Time.deltaTime * 5);
